a583b99e999c3df422773458b18684398b669941
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2018-03-24 13:11:02 -0700

    add two-way data binding

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,5 +1,11 @@
 studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
-  $scope.students = [];
+  $scope.students = [
+    { name: "Sam Schmidt" },
+    { name: "Jessica Martin" },
+    { name: "Sandy Smith" },
+    { name: "Ryan Samuels" },
+    { name: "Brentwood Davis" }
+  ];
   $scope.addStudent = function() {
     $scope.students.push({ name: $scope.studentName });
     // resets the `studentName` model to `null` so that the field in the HTML is cleared
